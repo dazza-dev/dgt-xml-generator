c04135145985351d3d7963a84980ea5e7592cf13
Dazza <adaza90@outlook.com>
2025-11-21 20:35:03 -0500

    Update Summary.php

--- src/Models/Summary.php
+++ src/Models/Summary.php
@@ -5,6 +5,16 @@ namespace DazzaDev\DgtXmlGenerator\Models;
 class Summary
 {
     /**
+     * Total servicios gravados con IVA
+     */
+    private ?float $totalTaxedServices = null;
+
+    /**
+     * Total servicios exentos de IVA
+     */
+    private ?float $totalExemptServices = null;
+
+    /**
      * Summary constructor
      *
      * @param  array  $data  Summary data
@@ -22,6 +32,46 @@ class Summary
         if (empty($data)) {
             return;
         }
+
+        if (isset($data['total_taxed_services'])) {
+            $this->setTotalTaxedServices($data['total_taxed_services']);
+        }
+
+        if (isset($data['total_exempt_services'])) {
+            $this->setTotalExemptServices($data['total_exempt_services']);
+        }
+    }
+
+    /**
+     * Get total taxed services
+     */
+    public function getTotalTaxedServices(): ?string
+    {
+        return $this->totalTaxedServices;
+    }
+
+    /**
+     * Set total taxed services
+     */
+    public function setTotalTaxedServices(float $totalTaxedServices): void
+    {
+        $this->totalTaxedServices = $totalTaxedServices;
+    }
+
+    /**
+     * Get total exempt services
+     */
+    public function getTotalExemptServices(): ?float
+    {
+        return $this->totalExemptServices;
+    }
+
+    /**
+     * Set total exempt services
+     */
+    public function setTotalExemptServices(float $totalExemptServices): void
+    {
+        $this->totalExemptServices = $totalExemptServices;
     }
 
     /**
@@ -29,6 +79,9 @@ class Summary
      */
     public function toArray(): array
     {
-        return [];
+        return [
+            'total_taxed_services' => $this->getTotalTaxedServices(),
+            'total_exempt_services' => $this->getTotalExemptServices(),
+        ];
     }
 }
